71a121e0f5f75ebac64530db1bd6c08841119d1e
Bertrand Benoit <contact@bertrand-benoit.net>
2011-01-06 13:30:29 +0000

    Removed bad characters.

--- Scripts/checkEpisodes.sh
+++ Scripts/checkEpisodes.sh
@@ -21,15 +21,15 @@ function usage() {
 }
 
 # usage : showEpisode <episode number> <position> <warning>
-# <position>: 1 for first, 2 for last, 0 otherwise.
+# <position>: 1 for first, 2 for last, 0 otherwise.
 # <warning>: 1 if this is a warning (shown in red), 0 otherwise.
 function showEpisode() {
-  # Prefixs if needed.
+  # Prefixs if needed.
   if [ $2 -ne 1 ]; then
     echo -ne " . "
   fi
 
-  # Enhances text if needed.
+  # Enhances text if needed.
   if [ $3 -eq 1 ]; then
     if [ $color -eq 1 ]; then
       echo -ne "\033[31m\033[4m"
@@ -38,10 +38,10 @@ function showEpisode() {
     fi
   fi
 
-  # Shows episode number.
+  # Shows episode number.
   echo -ne "$1"
 
-  # Enhances text if needed.
+  # Enhances text if needed.
   if [ $3 -eq 1 ]; then
     if [ $color -eq 1 ]; then
       echo -ne "\033[0m"
@@ -50,7 +50,7 @@ function showEpisode() {
     fi
   fi
 
-  # Enhances text if needed.
+  # Enhances text if needed.
   if [ $2 -eq 2 ]; then
     echo -ne "\n"
   fi
@@ -73,7 +73,7 @@ while [ "$1" != "" ]; do
   elif [ "$1" == "--pattern" ]; then
     shift
     pattern="$1"
-  elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     usage;
   else
     echo "Unknown parameter '$1'"
@@ -83,7 +83,7 @@ while [ "$1" != "" ]; do
   shift
 done
 
-# Ensures needed variables are defined.
+# Ensures needed variables are defined.
 if [ -z "$directory" ]; then
   echo "Must defined the directory to manage."
   usage;
@@ -100,7 +100,7 @@ currentNumber=-1
 #                Performs checks.
 #####################################################
 # For each corresponding file.
-# Algorithm:
+# Algorithm:
 #  - ls + grep -> gets files corresponding to pattern, one per line
 #  - first sed -> attempts extracting episode number (do not allow a dot before the episode number)
 #  - seconc grep -> removes each line which has something else than a number
@@ -108,7 +108,7 @@ currentNumber=-1
 moreThanOneEpisode=0
 for episodeNumberRaw in $( find "$directory" -maxdepth 1 -type f |grep -v "directory.lock" |grep -re "[0-9]" |sed -e 's/H264//g;s/(x264//g;s/Narvallo94//g;s/Drago3009//g;s/.*[^0-9.[-]\([0-9][0-9]*[-]*[0-9][0-9]*\)[^]a-fA-F0-9].*/\1/g;' |grep -v "[^0-9-]" |sort -n ); do
 
-  # Prints information if in debug mode.
+  # Prints information if in debug mode.
   if [ $debug = 1 ]; then
     echo -e "episodeNumber '$episodeNumberRaw'"
   fi
@@ -131,12 +131,12 @@ for episodeNumberRaw in $( find "$directory" -maxdepth 1 -type f |grep -v "direc
 
     continue
   elif [ $currentNumber -ge $episodeNumber ]; then
-    # Continues if the current episode number has already been managed (if there is several
+    # Continues if the current episode number has already been managed (if there is several
     #  files having the same number for instance).
     continue
   fi
 
-  # Defines there is more than one found episode.
+  # Defines there is more than one found episode.
   moreThanOneEpisode=1
 
   # Checks if there was a '-'.
@@ -163,10 +163,10 @@ if [ $currentNumber -eq -1 ]; then
   echo "none."
 else
   if [ $moreThanOneEpisode -eq 1 ] && [ -z "$lastEpisodeNumber" ]; then
-    # Shows the last episode (if not the only one, which has already been shown).
+    # Shows the last episode (if not the only one, which has already been shown).
     showEpisode $currentNumber 2 0
   else
-    # Moves to next line.
+    # Moves to next line.
     echo ""
   fi
 fi
