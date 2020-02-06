dff8c033222372491103c11cc1092cebc745ff80
Bertrand Benoit <contact@bertrand-benoit.net>
2020-02-06 17:49:10 +0100

    Updated checkEpisodes.sh script to adapt to shellcheck feedbacks (replacing for by while read, removing cat usage, improved some safe-guards).

--- checkEpisodes.sh
+++ checkEpisodes.sh
@@ -22,10 +22,9 @@ GREATER_EPISODE_NUMBER=999
 #####################################################
 # usage : usage
 function usage() {
-  echo "Usage: $0 [-h|--help] --dir|--allDir <directory> [--pattern <pattern>] [--debug] [--nocolor] [--warning]"
+  echo "Usage: $0 [-h|--help] --dir|--allDir <directory> [--debug] [--nocolor] [--warning]"
   echo -e "-h|--help\tshow this help"
   echo -e "<directory>\tdirectory to manage (with --dir), or parent directory (with --allDir, to check ALL sub-directories !)"
-  echo -e "<pattern>\tepisode files pattern (default is 'avi')"
   echo -e "--debug\t\tshow found line and links and does nothing more"
   echo -e "--nocolor\tdisable the warning color"
   echo -e "--warning\tshow warning message (more legible), and disable the warning color"
@@ -38,7 +37,6 @@ color=1
 warningMessage=0
 checkFirstNumber=0
 allDir=0
-pattern="avi"
 while [ "${1:-}" != "" ]; do
   if [ "$1" == "--debug" ]; then
     debug=1
@@ -57,9 +55,6 @@ while [ "${1:-}" != "" ]; do
     shift
     directory="$1"
     allDir=1
-  elif [ "$1" == "--pattern" ]; then
-    shift
-    pattern="$1"
   elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     usage;
   else
@@ -78,16 +73,18 @@ done
 #                Functions.
 #####################################################
 # usage : showEpisode <episode number> <position> <warning>
-# # <position>: 1 for first, 2 for next, 0 otherwise.
+# <position>: 1 for first, 2 for next, 0 otherwise.
 # <warning>: 1 if this is a warning (shown in red), 0 otherwise.
 function showEpisode() {
+  local _episodeNumber="$1" _position="$2" _warning="${3:-}"
+
   # Prefixs if needed.
-  if [ $2 -ne 1 ]; then
+  if [ "$_position" -ne 1 ]; then
     echo -ne " . "
   fi
 
   # Enhances text if needed.
-  if [ $3 -eq 1 ]; then
+  if [ "$_warning" -eq 1 ]; then
     if [ $color -eq 1 ]; then
       echo -ne "\033[31m\033[4m"
     else
@@ -97,10 +94,10 @@ function showEpisode() {
   fi
 
   # Shows episode number.
-  echo -ne "$1"
+  echo -ne "$_episodeNumber"
 
   # Enhances text if needed.
-  if [ $3 -eq 1 ]; then
+  if [ "$_warning" -eq 1 ]; then
     if [ $color -eq 1 ]; then
       echo -ne "\033[0m"
     else
@@ -109,7 +106,7 @@ function showEpisode() {
   fi
 
   # Enhances text if needed.
-  if [ $2 -eq 2 ]; then
+  if [ "$_position" -eq 2 ]; then
     echo -ne "\n"
   fi
 }
@@ -122,50 +119,49 @@ function checkDirectory() {
 
   # Extracts episode number of files an outputs them in a temporary file.
   rm -f "$episodeNumberFile"
-  for filePathRaw in $( find "$_directory" -maxdepth 1 -type f |grep -v "directory.lock" |grep -e "\/[^/]*[0-9][^/]*$" |sed -e 's/[ ]/€/g;' ); do
-    filePath=$( echo "$filePathRaw" |sed -e 's/€/ /g;' )
+  while IFS= read -r filePath; do
     fileName=$( basename "$filePath" )
     episodeNumber=$( extractEpisodeNumber "$fileName" )
     # TODO: add filename in file, and extracts it after that, to improve debug info
     isCompoundedNumber "$episodeNumber" && echo "$episodeNumber" >> "$episodeNumberFile" && continue
     echo "WARNING: Unable to extract (single or compounded) episode number from '$fileName' (result: $episodeNumber)"
-  done
+  done < <(find "$_directory" -maxdepth 1 -type f |grep -v "directory.lock" |grep -e "\/[^/]*[0-9][^/]*$")
 
   # Ensures there is result.
   [ ! -f "$episodeNumberFile" ] && echo "none" && return 0
 
   # Ensures the first number is 1 (or a compounded number beginning with 1).
   if [ $checkFirstNumber -eq 1 ]; then
-    firstNumber=$( cat $episodeNumberFile |sort -n |head -n 1 |sed -e "s/^\([^-]*\)-.*$/\1/;" )
-    [ $( echo "$firstNumber" |grep -ce "^[0]*1" ) -lt 1 ] && echo "WARNING: first number is NOT 1 ($firstNumber)"
+    firstNumber=$( sort -n "$episodeNumberFile" |head -n 1 |sed -e "s/^\([^-]*\)-.*$/\1/;" )
+    [ "$( grep -ce "^[0]*1"<<<"$firstNumber" )" -lt 1 ] && echo "WARNING: first number is NOT 1 ($firstNumber)"
   fi
 
   # Works on episode number of the temporary file.
   moreThanOneEpisode=0
-  for episodeNumberRaw in $( cat $episodeNumberFile |sort -n ); do
+  while IFS= read -r episodeNumberRaw; do
     # Prints information if in debug mode.
-    if [ $debug = 1 ]; then
+    if [ "$debug" = 1 ]; then
       echo -e "episodeNumber '$episodeNumberRaw'"
     fi
 
     # Manages potential '-' into episode number(s), taking the last one.
-    lastEpisodeNumber=$( echo $episodeNumberRaw |grep "-" |sed -e "s/.*-\([0-9]*\)/\1/;" )
-    if [ ! -z $lastEpisodeNumber ]; then
-      episodeNumber=$lastEpisodeNumber
+    lastEpisodeNumber=$( echo "$episodeNumberRaw" |grep "-" |sed -e "s/.*-\([0-9]*\)/\1/;" )
+    if [ -n "$lastEpisodeNumber" ]; then
+      episodeNumber="$lastEpisodeNumber"
     else
-      episodeNumber=$episodeNumberRaw
+      episodeNumber="$episodeNumberRaw"
     fi
 
     # safe-guard.
-    [ $episodeNumber -gt $GREATER_EPISODE_NUMBER ] && echo "WARNING: Ignoring too big episode number $episodeNumber ..." && continue
+    [ "$episodeNumber" -gt "$GREATER_EPISODE_NUMBER" ] && echo "WARNING: Ignoring too big episode number $episodeNumber ..." && continue
 
     # Updates the current episode number.
-    if [ $currentNumber -eq -1 ]; then
-        currentNumber=$episodeNumber
-        showEpisode $episodeNumberRaw 1 0
+    if [ "$currentNumber" -eq -1 ]; then
+        currentNumber="$episodeNumber"
+        showEpisode "$episodeNumberRaw" 1 0
 
       continue
-    elif [ $currentNumber -ge $episodeNumber ]; then
+    elif [ "$currentNumber" -ge "$episodeNumber" ]; then
       # Continues if the current episode number has already been managed (if there is several
       #  files having the same number for instance).
       continue
@@ -175,36 +171,44 @@ function checkDirectory() {
     moreThanOneEpisode=1
 
     # Checks if there was a '-'.
-    if [ ! -z $lastEpisodeNumber ]; then
-      showEpisode $episodeNumberRaw 0 0
-      currentNumber=$lastEpisodeNumber
+    if [ -n "$lastEpisodeNumber" ]; then
+      showEpisode "$episodeNumberRaw" 0 0
+      currentNumber="$lastEpisodeNumber"
       continue
     fi
 
     # Performs check.
-    awaitedNumber=$( expr $currentNumber + 1 )
+    awaitedNumber=$((currentNumber + 1))
     currentNumber=$awaitedNumber
-    if [ $awaitedNumber -lt $episodeNumber ]; then
+    if [ "$awaitedNumber" -lt "$episodeNumber" ]; then
       # Shows each missing episode number.
-      while [ $currentNumber -lt $episodeNumber ]; do
-        showEpisode $currentNumber 0 1
-        currentNumber=$( expr $currentNumber + 1 )
+      while [ "$currentNumber" -lt "$episodeNumber" ]; do
+        showEpisode "$currentNumber" 0 1
+        currentNumber=$((currentNumber + 1))
       done
     fi
-  done
+  done < <(sort -n "$episodeNumberFile")
 
   # Shows the last episode number if any, "none" otherwise.
-  if [ $currentNumber -eq -1 ]; then
+  if [ "$currentNumber" -eq -1 ]; then
     echo "none."
   else
-    if [ $moreThanOneEpisode -eq 1 ] && [ -z "$lastEpisodeNumber" ]; then
+    if [ "$moreThanOneEpisode" -eq 1 ] && [ -z "$lastEpisodeNumber" ]; then
       # Shows the last episode (if not the only one, which has already been shown).
-      showEpisode $currentNumber 2 0
+      showEpisode "$currentNumber" 2 0
     else
       # Moves to next line.
       echo ""
     fi
   fi
+}
+
+# usage: extractEpisodeNumber <file name>
+function extractEpisodeNumber() {
+  # - finally extracts numbers
+  echo "$1" \
+  |sed -e 's/^.*[^0-9.[-]\([0-9][0-9]*[-]*[0-9][0-9]*\)[^]a-fA-F0-9].*$/\1/g;' \
+  |sed -e 's/^0*//g;s/-0*/-/g;s/^.*[.]\([0-9][0-9]*[-]*[0-9]*\)[.].*$/\1/g;'
 }
 
 #####################################################
@@ -215,10 +219,9 @@ if [ $allDir -eq 0 ]; then
   checkDirectory "$directory"
 else
   # Works on any found sub-directories.
-  for subDirRaw in $( find "$directory" -maxdepth 1 -type d |grep -v "^$directory\$" |sed -e 's/[ \t]/€/g;' |sort ); do
-    subDir=$( echo "$subDirRaw" |sed -e 's/€/ /g;' )
+  while IFS= read -r subDir; do
     subDirName=$( echo "$subDir" |sed -e 's/\/mnt\/[^\/]*\///' )
     echo -ne "Checking $subDirName: "
     checkDirectory "$subDir"
-  done
+  done < <(find "$directory" -maxdepth 1 -type d |grep -v "^$directory\$" |sort)
 fi
